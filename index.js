dbe130607ae24094ee90549c9732150061bbe9f9
hefi1413 <hefi1413@gmail.com>
2022-03-23 16:16:55 -0300

    commit 1

---
+++ index.js
@@ -0,0 +1,134 @@
+
+// constantes e variaveis do app
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 3000;
+
+var pokedex = [];
+var message = "";
+
+
+
+// app functions
+
+// procura o pokemon no array
+function findPokemon(index) {
+
+    let result =null;
+    if(index > pokedex.length-1) {
+        return result;
+    };
+    return pokedex[index];
+}
+
+// adiciona pokemons no array
+function adicionaPokemon(pokemon) {
+
+    pokedex.push( pokemon );
+    pokemon.id =pokedex.length-1;
+
+}
+
+// carrega os pokemons no array
+function loadPokedex() {
+
+    if( pokedex.length > 0 ) {
+        return;
+    };
+
+    adicionaPokemon( { 'nome': 'Bulbasaur',
+                    'numero': 1,
+                    'tipo': 'poison',
+                    'imagem': '/img/001.png',
+                    'descricao':'Há uma semente de planta em suas costas desde o dia que nasceu' } );
+    
+    adicionaPokemon( { 'nome': 'Charmander',
+                    'numero': 4,
+                    'tipo': 'fire',
+                    'imagem': '/img/004.png',
+                    'descricao':'Tem preefrencia por oisas quentes.' } );
+
+    adicionaPokemon( { 'nome': 'Charizzard',
+                    'numero': 6,
+                    'tipo': 'fly',
+                    'imagem': '/img/006.png',
+                    'descricao':'PIt spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames' } );
+
+}
+
+// app configuration
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: false}));
+
+
+
+// GET - render principal
+app.get("/", function (req, res) {
+
+    loadPokedex();
+
+    setTimeout(() => {
+        message = "";
+      }, 1000)
+
+    res.render("index", { 'pokedex': pokedex, 'title':'Pokedéx', 'message': message } );
+});
+
+// GET - cadastramento
+app.get("/cadastrar", function (req, res) {
+    res.redirect("/");
+});
+
+// GET - render cadastramento
+app.get("/cadastrar/render", function (req, res) {
+    res.render("cadastrar");
+});
+
+// POST - adicionar (new)
+app.post("/cadastrar/new", function (req, res) {
+    
+    let pokeData =req.body;
+
+    // adiciona dados do pokemon no array
+    adicionaPokemon( pokeData );
+
+    //console.log( 'pokeData:', pokeData);
+
+    // pokemon adicionado com sucesso
+    message = `Sucesso!. O pokemon ${pokeData.nome} foi adicionado à lista`;
+        
+    // exibe tela com novo pokemon
+    res.redirect("/");
+
+});
+
+// GET - detalhes
+app.get("/detalhes", function (req, res) {
+    res.redirect("/");
+});
+
+// GET - detalhes do pokemon
+app.get("/detalhes/:id", function (req, res) {
+
+    let id =req.params.id;
+
+  //  console.log('id:', id);
+
+    // localizar pokemon
+    let pokemon = findPokemon(id);
+
+    if( ! pokemon ) {
+        pokemon = {};
+        pokemon.nome = '';
+        pokemon.numero =0;
+        pokemon.descricao ='Este pokemon não existe. :(';
+    }
+    res.render("detalhes", {'pokemon': pokemon} );
+});
+
+// servidor
+app.listen(port, () => {
+    console.log(`App listening on port: ${port}`);
+});
---
+++ public/js/script.js
@@ -0,0 +1,162 @@
+
+var  message =null;
+
+window.onload = function() {
+
+    message =document.querySelector("#message");
+
+    setTimeout(() => {
+        message.style.display = "none";
+    }, 5000);
+}
+
+
+// associa click ao botão close da messsge
+$(document).on('click', '#close', function() {
+    console.log('script.close');
+
+    message.style.display = "none";
+
+});
+
+
+// associa click ao container do pokemon (detalhes)
+$(document).on('click', '.card', function() {
+    console.log('script.click');
+
+    // recupera o numero do pokemon
+    let divContainer = this;
+    let idPokemom = divContainer.getAttribute('data-id');
+    let postData =null;
+
+    //console.log('idPokemom:', idPokemom);
+    
+    // GET - dados do pokemon
+    $.ajax({
+        url: '/detalhes/'+idPokemom,
+        method: "get",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(postData),
+        success: successDetalhes,
+        error: errorDetalhes
+    });
+
+});
+
+
+// associa click ao menu CADASTRAR
+$(document).on('click', '#menu-cadastrar', function(event) {
+    console.log('script.cadastrar');
+
+    event.preventDefault();
+        
+    let postData =null;
+    // POST com dados do pokemon
+    $.ajax({
+        url: '/cadastrar/render',
+        method: "get",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(postData),
+        success: successCadastrar,
+        error: errorCadastrar
+    });
+
+});
+
+/*
+// associa click ao botão adicionar
+$(document).on('click', '', function(event) {
+    console.log('script.new');
+
+    event.preventDefault();
+        
+    let nome =document.querySelector('#nome').value;
+    let numero =document.querySelector('#numero').value;
+    let tipo =document.querySelector('#tipo').value;
+    let imagem =document.querySelector('#imagem').value;
+    let descricao =document.querySelector('#descricao').value;
+    let altura =document.querySelector('#altura').value;
+    let peso =document.querySelector('#peso').value;
+    let categoria =document.querySelector('#categoria').value;
+    let habilidade =document.querySelector('#habilidade').value;
+
+    let postData ={'nome': nome, 'numero': numero, 'tipo': tipo, 'imagem': imagem, 'descricao': descricao, 
+            'altura': altura, 'peso': peso, 'categoria': categoria, 'habilidade': habilidade };
+    // POST com dados do pokemon
+    $.ajax({
+        url: '/cadastrar/new',
+        method: "post",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(postData),
+        success: successCadastrar,
+        error: errorCadastrar
+    });
+
+});
+    */
+
+// evento de sucesso da resposta DETALHES
+function successDetalhes(result) {
+    console.log('script.successDetalhes');
+
+    /*
+    // torna o container 'content' invisible
+    document.querySelector('#content').style.display = 'none';
+    document.querySelector('#cadastrar').style.display = 'none';
+    // torna o container 'detalhes' visible
+    let detalhes = document.querySelector('#detalhes');
+    detalhes.innerHTML = result;
+    detalhes.style.display = 'flex';
+    */
+
+    // exibe o resultado
+    let content = $('#content');
+    content.css('display','none');
+    content.removeClass('main');
+    content.addClass('details');
+    content.html( result );
+    content.css('display','flex');
+}
+
+// evento de erro da resposta DETALHES
+function errorDetalhes(result) {
+    console.log('script.errorDetalhes');
+    
+}
+
+
+// evento de sucesso da resposta de CADASTRAR
+function successCadastrar(result) {
+    console.log('script.successCadastrar');
+
+    /*
+    // torna o container 'detalhes' invisible
+    document.querySelector('#content').style.display = 'none';
+    document.querySelector('#detalhes').style.display = 'none';
+
+    // torna o container 'register' (cadastro) visible
+    let cadastrar = document.querySelector('#cadastrar');
+
+    console.log('result : ', result);
+
+    cadastrar.innerHTML = result;
+    cadastrar.style.display = 'block';
+    */
+
+    console.log('result : ', result);
+
+    // exibe o resultado
+    let content = $('#content');
+    content.css('display','none');
+    content.removeClass('main');
+    content.addClass('add');
+    content.html( result );
+    content.css('display','block');
+
+}
+
+// evento de erro da resposta CADASTRAR
+function errorCadastrar(err) {
+    console.log('script.errorCadastrar');
+    
+}
